02fb166dd17b9918d21e6a53b2a463449e139bde
Junho-06 <k34920032@gmail.com>
2022-07-05 10:43:23 +0900

    Chrome

--- Chrome/js/quotes.js
+++ Chrome/js/quotes.js
@@ -67,3 +67,8 @@ const quotes = [
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quotes;
+author.innerText = todaysQuote.author;
