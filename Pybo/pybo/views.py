4ae50b55cf1d1b3e369a87aa3bec9253619ad150
Junho-06 <k34920032@gmail.com>
2022-07-17 09:21:08 +0900

    페이징

--- Pybo/pybo/views.py
+++ Pybo/pybo/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Question, Answer
+from .models import Question
 from .forms import QuestionForm, AnswerForm
-from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 
 
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
